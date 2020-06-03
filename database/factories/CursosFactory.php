d7eb4c06f9d439fb3515faf9370300a355644c2c
Pau Gonzàlez Martí <lazytime96@gmail.com>
2020-06-03 14:32:56 +0200

    Creats els models de Usuari i Curs

---
+++ database/factories/CursosFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Cursos;
+use Faker\Generator as Faker;
+
+$factory->define(Cursos::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
